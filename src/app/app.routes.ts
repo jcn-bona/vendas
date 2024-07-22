fd954bd9c5cc9e555257083a09463028a0a6601c
jeronimo cardoso neto <jeronimo.cardoso.neto@gmail.com>
2024-07-22 10:59:45 -0300

    ajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
-import {Routes} from '@angular/router'
-import {CategoriasComponent} from './categorias/categorias.component'
+import { Routes } from '@angular/router'
+import { CategoriasComponent } from './categorias/categorias.component'
 import { DashboardComponent } from './dashboard/dashboard.component'
 
 export const routes: Routes = [
     { path: 'categorias', component: CategoriasComponent },
-    
     { path: '', component: DashboardComponent }
 ]
